925bc80b2f9853495a11ef487182a133d5e53048
rl <richard.latchoumane@ansamb.com>
2022-04-05 13:34:09 +0400

    feat(debuggable): property wrappers

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Debuggable
 
-A description of this package.
+Property Wrapper for setting up different default value for all environments
+
+```swift
+
+@Debuggable(debug: "debug", preview: "swiftui preview", testing: "Unit test")
+static var shared: String = "release"
+
+```
--- Sources/Debuggable/Debuggable.swift
+++ Sources/Debuggable/Debuggable.swift
@@ -1,6 +1,47 @@
-public struct Debuggable {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+@propertyWrapper
+public struct SingletonDebuggable<Value> {
+    #if DEBUG
+    public var wrappedValue: Value
+    #else
+    public let wrappedValue: Value
+    #endif
+    
+    public init(wrappedValue: Value, debug: Value? = nil, preview: Value? = nil, testing: Value? = nil) {
+        #if DEBUG
+        self.wrappedValue = {
+          if (ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1") {
+              return preview ?? wrappedValue
+          } else {
+              return debug ?? wrappedValue
+          }
+        }()
+        #elseif TESTING
+        self.wrappedValue = testing ?? wrappedValue
+        #else
+        self.wrappedValue = wrappedValue
+        #endif
+    }
+}
+
+@propertyWrapper
+public struct Debuggable<Value> {
+    public var wrappedValue: Value
+    
+    public init(wrappedValue: Value, debug: Value? = nil, preview: Value? = nil, testing: Value? = nil) {
+        #if DEBUG
+        self.wrappedValue = {
+          if (ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1") {
+              return preview ?? wrappedValue
+          } else {
+              return debug ?? wrappedValue
+          }
+        }()
+        #elseif TESTING
+        self.wrappedValue = testing ?? wrappedValue
+        #else
+        self.wrappedValue = wrappedValue
+        #endif
     }
 }
